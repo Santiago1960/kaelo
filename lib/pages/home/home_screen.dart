ab3b3a2677f721c300b83afa30982507dc986b5d
Santiago Caicedo <caicedo.santiago@gmail.com>
2025-08-05 13:16:32 -0500

    250805 Listo botones principales

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kaelo/widgets/custom_footer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,7 +9,8 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Text('Página de inicio!'),
-      )
+      ),
+      bottomNavigationBar: CustomFooter()
     );
   }
 }
---
+++ lib/widgets/custom_footer.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CustomFooter extends StatelessWidget {
+  const CustomFooter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final screenWidth = MediaQuery.of(context).size.width;
+
+
+    return Container(
+      color: Colors.white,
+      height: 120,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+
+          Container(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 230, 246, 252),
+              borderRadius: BorderRadius.all(Radius.circular(12.0))
+            ),
+            height: screenWidth * 0.25,
+            width: screenWidth * 0.25,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.thumb_up_sharp, color: Colors.green,),
+                Text('Sí', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                Icon(Icons.check_circle, color: Colors.green,)
+              ],
+            )
+          ),
+
+          Container(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 217, 216, 216),
+              borderRadius: BorderRadius.all(Radius.circular(12.0))
+            ),
+            height: screenWidth * 0.25,
+            width: screenWidth * 0.25,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: SvgPicture.asset(
+                'assets/icon/confused.svg',
+                width: 60,
+              ),
+            ),
+          ),
+
+          Container(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 230, 246, 252),
+              borderRadius: BorderRadius.all(Radius.circular(12.0))
+            ),
+            height: screenWidth * 0.25,
+            width: screenWidth * 0.25,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.thumb_down_sharp, color: Colors.red,),
+                Text('No', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                Icon(Icons.cancel_rounded, color: Colors.red,)
+              ],
+            )
+          ),
+        ],
+      )
+    );
+  }
+}
